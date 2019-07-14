4e3df2e9d0d9a346c488deb5f558baf63f41fc40
cc <18361434606@163.com>
2019-07-14 21:35:30 +0800

    hotfix

--- test.js
+++ test.js
@@ -4,3 +4,27 @@ var P;
     P[P["age"] = 18] = "age";
 })(P || (P = {}));
 console.log(P.name);
+function getHero(age, type) {
+    if (type !== undefined) {
+        return '找到了' + age + type + '的英雄';
+    }
+    return '找到了' + age + '的英雄';
+}
+console.log(getHero(18, 'ironMan'));
+console.log(getHero(18));
+function getName() {
+    var age = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        age[_i] = arguments[_i];
+    }
+    var uu = '找到了';
+    age.forEach(function (items, index) {
+        uu = uu + items;
+        if (index < age.length - 1) {
+            uu = uu + '.';
+        }
+    });
+    uu = uu + '的英雄';
+    return uu;
+}
+console.log(getName('sp', 'icon', 'hha'));
--- test.ts
+++ test.ts
@@ -1,2 +1,27 @@
 enum P { name = 'chen', age = 18 }
 console.log(P.name);
+
+function getHero(age: number, type?: string): string {
+  if (type !== undefined) {
+    return '找到了' + age + type + '的英雄'
+  }
+  return '找到了' + age + '的英雄'
+}
+
+console.log(getHero(18, 'ironMan'));
+console.log(getHero(18));
+
+
+function getName(...age: string[]): string {
+  let uu = '找到了'
+  age.forEach((items, index) => {
+    uu = uu + items
+    if (index < age.length - 1) {
+      uu = uu + '.'
+    }
+  })
+  uu = uu + '的英雄'
+  return uu
+
+}
+console.log(getName('sp', 'icon', 'hha'));
